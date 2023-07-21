57a6ba3433edf35800ba4c55c6e4b60be5f3b994
acardellini <82385740+acardellini@users.noreply.github.com>
2023-07-21 11:45:11 +0200

    Add files via upload

---
+++ CG/6CP-NT/PP/PD/pp_initial.m
@@ -0,0 +1,109 @@
+clear all
+%close all
+t=7;       %total tubes COmbinazione= 190 cases
+nt = linspace(1,t,t);
+r=0;
+y=0;
+g=0;
+nc0 = 0;
+%% plot
+% Create figure
+figure1 = figure(1);
+
+% Create axes
+axes1 = axes('Parent',figure1);
+hold(axes1,'on');
+% Create ylabel
+ylabel('# TRP-TRP contacts ');
+
+% Create xlabel
+xlabel('cos(\Theta)');
+
+box(axes1,'on');
+% Set the remaining axes properties
+set(axes1,'FontSize',18);
+%%
+
+for i = 1:t
+    j = i+1;
+    l=t-i;
+    ll=0;
+    nc=ones(1,l);
+    cosa=ones(1,l);
+    while j <= 7
+        ll=ll+1;
+        nti = strcat('nt',num2str(i),'-');
+        ntj = strcat('nt',num2str(j));
+        cd ncont
+        filename = strcat(nti,ntj,'.xvg');
+        NC = importdata(filename,' ',24);
+        NCdata= NC.data(:,2);
+        NC_mean = mean(NCdata(200:250));
+        NC_std = std(NCdata(200:250));
+        nc(ll)=NC_mean;
+    cd ../nvect
+    filename = strcat('vect_',nti,ntj,'.xvg');
+    angle = importdata(filename,' ',24);
+    Adata = angle.data(:,2);
+    A_mean = mean(Adata(200:250));
+    A_std =  std(Adata(200:250));
+    Ar_mean=2*pi*A_mean/360;
+    cosa(ll)=abs(cos(Ar_mean));
+
+    j = j+1;
+    cd ..
+    end
+% Create figure
+figure(1)
+for k = 1:length(cosa)
+    if nc(k)==0
+        nc0 = nc0+1;
+     end
+    if cosa(k)>=0.9
+        r=r+1;
+        plot(cosa(k),nc(k),'MarkerFaceColor',[0.8 0.8 0.78],...
+            'MarkerEdgeColor',[0 0 0],...
+            'MarkerSize',20,...
+            'Marker','o',...
+            'LineStyle','none',...
+            'Color',[0.8 0.8 0.78]);
+        
+    elseif cosa(k) < 0.9 && cosa(k)>=0.5 
+        g=g+1;
+        plot(cosa(k),nc(k),'MarkerFaceColor',[0.8 0.8 0.78],...
+            'MarkerEdgeColor',[0 0 0],...
+            'MarkerSize',20,...
+            'Marker','o',...
+            'LineStyle','none',...
+            'Color',[0.8 0.8 0.78]);
+    elseif   cosa(k)<0.5 
+        y=y+1;
+        plot(cosa(k),nc(k),'MarkerFaceColor',[0.8 0.8 0.78],...
+            'MarkerEdgeColor',[0 0 0],...
+            'MarkerSize',20,...
+            'Marker','o',...
+            'LineStyle','none',...
+            'Color',[0.8 0.8 0.78]);
+    end
+end
+figure(2);
+for k = 1:length(cosa)
+    if nc(k)==0
+        nc0 = nc0+1;
+     end
+    if cosa(k)>=0.85
+        Adata =2*pi*Adata/360;
+        costheta = abs(cos(Adata));
+        time = angle.data(:,1);
+        plot(time,costheta);hold on;
+    end
+end
+
+hold on
+clear cosa nc
+end
+fr=r/45;
+fg=g/45;
+fy=y/45;
+fnc = nc0/45;
+    
---
+++ CG/6CP-NT/PP/PD/pp_nt_new.sh
@@ -0,0 +1,31 @@
+# @ shell=/bin/bash
+gmx make_ndx -f INPUT/conf_nt.gro -o INPUT/index.ndx < INPUT/nt.txt
+gmx make_ndx -f INPUT/conf_nt.gro -o INPUT/index_vect.ndx < INPUT/input_index_vect.txt
+mkdir ncont nvect
+
+for c in {1..10}
+do
+nt=$c
+ntt=$(bc <<< "${c}+1")
+
+cd ncont
+while [ ${ntt} -le 10 ]
+do
+gmx mindist -f ../../../MD/10nt_md.xtc -s ../INPUT/input_nt.tpr -n ../INPUT/index.ndx -e 8000000 -on nt${nt}-nt${ntt}.xvg -group << EOF
+TRP${nt}
+TRP${ntt}
+EOF
+ntt=$((ntt+1))
+done
+rm *#
+
+cd ../nvect
+ntt=$(bc <<< "${c}+1")
+while [ ${ntt} -le 10 ]
+do
+gmx gangle -f ../../../MD/10nt_md.xtc -s ../INPUT/input_nt.tpr -n ../INPUT/index_vect.ndx -e 8000000 -g1 vector -group1 'com of group 'cp${nt}1' plus com of group 'cp${nt}2'' -g2 vector -group2 'com of group 'cp${ntt}1' plus com of group 'cp${ntt}2'' -oav vect_nt${nt}-nt${ntt}.xvg -oh histo_nt${nt}-nt${ntt}.xvg
+ntt=$((ntt+1))
+done
+rm *#
+cd ..
+done
